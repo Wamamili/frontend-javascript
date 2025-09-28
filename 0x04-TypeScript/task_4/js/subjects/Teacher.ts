ea9fc480bc2a5468db380d1c2a469e4f864e806e
wamamili <wamamiliclean@gmail.com>
2025-09-28 12:10:55 +0300

    Declaration

--- 0x04-TypeScript/task_4/js/subjects/Teacher.ts
+++ 0x04-TypeScript/task_4/js/subjects/Teacher.ts
@@ -4,4 +4,12 @@ namespace Subjects {
         firstName: string;
         lastName: string;
     }
+
+    export class Subject {
+        teacher: Teacher;
+
+        setTeacher(teacher: Teacher) {
+            this.teacher = teacher;
+        }
+    }
 }
